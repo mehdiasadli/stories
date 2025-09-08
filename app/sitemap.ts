a46056f1b608edd8cfb9ca929b37bea58e5c22fd
Mehdi Asadli <mehdiasadli@azericard.com>
2025-09-08 13:29:15 +0400

    update: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,35 +2,47 @@ import { MetadataRoute } from 'next';
 import { getAllChapters, getAllCharacters } from '@/lib/fetchers';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = process.env.NEXT_PUBLIC_APP_URL;
+
   const chapters = await getAllChapters();
   const characters = await getAllCharacters();
 
   const chapterEntries: MetadataRoute.Sitemap = chapters.map((chapter) => ({
-    url: `${process.env.NEXT_PUBLIC_APP_URL}/${chapter.slug}`,
+    url: `${url}/${chapter.slug}`,
     changeFrequency: 'monthly',
     lastModified: chapter.updatedAt,
   }));
 
   const characterEntries: MetadataRoute.Sitemap = characters.map((character) => ({
-    url: `${process.env.NEXT_PUBLIC_APP_URL}/characters/${character.slug}`,
+    url: `${url}/characters/${character.slug}`,
     changeFrequency: 'weekly',
     lastModified: character.updatedAt,
   }));
 
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/login`,
-      changeFrequency: 'never',
-      lastModified: new Date(),
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/register`,
-      changeFrequency: 'never',
-      lastModified: new Date(),
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/`,
+      url: `${url}/`,
       changeFrequency: 'monthly',
+      lastModified: new Date(),
+    },
+    {
+      url: `${url}/about`,
+      changeFrequency: 'yearly',
+      lastModified: new Date(),
+    },
+    {
+      url: `${url}/characters`,
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+    },
+    {
+      url: `${url}/auth/login`,
+      changeFrequency: 'never',
+      lastModified: new Date(),
+    },
+    {
+      url: `${url}/auth/register`,
+      changeFrequency: 'never',
       lastModified: new Date(),
     },
     ...chapterEntries,

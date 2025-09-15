036944c41053b8862f811b538f9610bc98f481a9
Mehdi Asadli <mehdiasadli@azericard.com>
2025-09-15 10:37:18 +0400

    update: robots

--- app/robots.ts
+++ app/robots.ts
@@ -3,9 +3,23 @@ import { MetadataRoute } from 'next';
 export default function Robots(): MetadataRoute.Robots {
   return {
     sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/chapters/*',
+      },
+      {
+        userAgent: '*',
+        allow: '/characters/*',
+      },
+      {
+        userAgent: '*',
+        disallow: '/dashboard/*',
+      },
+    ],
   };
 }
